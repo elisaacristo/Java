e6c51a7d132de544c0589014600389436bb13677
Elisa Araujo Cristo <105324780+elisaacristo@users.noreply.github.com>
2026-04-02 00:15:29 -0300

    Add Convert class to demonstrate type conversion

---
+++ Testes/convert.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Convert {
+	public static void main(String[]args) {
+		
+		double a = 2.4926;
+		int b = (int) a;
+		String c = "elisa";
+		System.out.println(a);
+		System.out.println(b);
+	}
+}
