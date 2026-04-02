d279552b016b4b6315fb986a84a0f1f27134936e
Elisa Araujo Cristo <105324780+elisaacristo@users.noreply.github.com>
2026-04-02 00:26:44 -0300

    Add temperature conversion from Fahrenheit to Celsius

---
+++ Códigos/temperatura.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class Temperatura {
+	public static void main(String[]args) {
+		Scanner ler = new Scanner(System.in);
+		double fah, cel;
+
+		System.out.println("Digite a temperatura em Fahrenheit: ");
+		fah = ler.nextDouble();
+
+		cel = (fah - 32) / 1.8;
+		System.out.println("Temperatura em Celsius: " + cel);
+
+		if (cel < 15) {
+			System.out.println("Temperatura: fria");
+		} else if (cel < 22) {
+			System.out.println("Temperatura: amena");
+		} else {
+			System.out.println("Temperatura: quente");
+		}
+	}
+}
