ce6aab172ece9261afc3c9120819fce463254b28
Elisa Araujo Cristo <105324780+elisaacristo@users.noreply.github.com>
2026-04-02 00:11:57 -0300

    Create teste.java
    Initial implementation of the teste class with main method and commented code snippets.

---
+++ OBI2024/teste.java
@@ -0,0 +1,41 @@
+import java.util.*;
+public class teste {
+	public static void main(String[]args) {
+		Scanner in = new Scanner(System.in);
+		
+		/*
+		 * ArrayList<String/Double/Integer> teste = new ArrayList();
+		 * teste.add("oiii");
+		 * teste.sort(Comparator.naturalOrder());
+		 * teste.sort(Comparator.reverseOrder());
+		 * remove
+		 * clear
+		 * get
+		 * 
+		 * primo:
+		 * for i
+		 * boolean primo = true
+		 * for j
+		 * i % j == 0
+		 * primo false
+		 * print i
+		 * 
+		 * meuTxtindexOf(,);
+		 * meuTxt.length();
+		 * String novotxt = meuTxt.split("[,\\s\\.]);
+		 * 
+		 * Math.pow();
+		 * Math.abs();
+		 * Math.sqrt()
+		 * Math.random() * 101
+		 * max
+		 * min
+		 * round
+		 * 
+		 * printf("%.2f", teste);
+		 * 
+		 * uppercase
+		 * lowercase
+		 */
+	}
+}
